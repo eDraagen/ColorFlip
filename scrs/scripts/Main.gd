e4be35aad6cf84a66187ee45e982c156c8e9180e
eDraagen <90107745+eDraagen@users.noreply.github.com>
2022-10-14 17:30:09 +0200

    Trying to fix screen_trans

--- scrs/scripts/Main.gd
+++ scrs/scripts/Main.gd
@@ -2,7 +2,7 @@ extends Node
 
 onready var manu = $Manu
 onready var signal_bus = $"/root/EventsAutoload"
-
+onready var screen_trans = $"/root/ScreenTransitionRect"
 onready var manuStartButton = $Manu/main_menu/startButton
 onready var manuQuitButton = $Manu/main_menu/quitButton
 
@@ -16,14 +16,13 @@ onready var scene_timer = $scene_timer
 
 func _ready():
 	
-	$"/root/EventsAutoload".emit_signal("fade_to_trans")
+	screen_trans._fade_to_trans()
 	#<--- Buttons connect signal --->
 	manuQuitButton.connect("pressed", self, "_quit_button")
 	manuStartButton.connect("pressed", self, "_start_button")
 	
 	
 	#Timer connect signal
-	anim_timer.connect("timeout", self, "_on_animation_timer_timeout")
 	scene_timer.connect("timeout", self, "_on_next_scene_timer_timeout")
 	
 	
@@ -32,18 +31,17 @@ func _quit_button():
 	get_tree().quit()
 	
 func _start_button():
-	$"/root/EventsAutoload".emit_signal("fade_to_black")
-	anim_timer.start(0.5)
+	screen_trans._fade_to_black()
 	scene_timer.start(1)
 	manu.hide()
 
 
 #<--- ON timeout change scene, when start button pressed --->
 func _on_next_scene_timer_timeout():
-	_next_scene()	
+	_next_scene()
+	screen_trans._fade_to_trans()
 	
 func _next_scene():
 	get_tree().change_scene("res://scrs/levels/Level1.tscn")
 
-func _on_animation_timer_timeout():
-	$"/root/EventsAutoload".emit_signal("fade_to_trans")
+
